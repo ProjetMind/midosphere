8081e3003e37d2075540a2ec1977557bd5ec7ec8
nsi <nsi@192.168.0.33>
2013-11-28 03:25:48 +0100

    - add use Assert

--- src/Mind/MediaBundle/Entity/ImageAvis.php
+++ src/Mind/MediaBundle/Entity/ImageAvis.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace Mind\MediaBundle\Entity;
-use Symfony\Component\Validator\Constraints as Assert;
 
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * ImageAvis
